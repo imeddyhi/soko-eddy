3994f66ceb290d4027b2ba535854192d3ff3de9b
imeddyhi <156390561+imeddyhi@users.noreply.github.com>
2024-02-22 22:56:20 -0600

    Update sokoban-by-eddy(v.0.).py

--- sokoban-by-eddy(v.0.).py
+++ sokoban-by-eddy(v.0.).py
@@ -2,6 +2,15 @@
 Versión de sokoban de eduardo
 version 0 (thu-02-22-2024)
 '''
-mapa = [3,4,4,0,4,4,4,3]
-print(mapa)
 
+# '0' representa el personaje
+# '1' representa una caja
+# '2' representa una meta
+# '3' representa una pared
+# '4' representa espacios/piso
+
+mapa = [3,4,4,0,4,4,4,3] # Se asigna un array a la variable mapa, crea una fila con 8 columnas donde el personaje podrá moverse laterlamente
+
+# Mostrar el mapa inicial
+print("Mapa del almacén:")
+print(mapa)
