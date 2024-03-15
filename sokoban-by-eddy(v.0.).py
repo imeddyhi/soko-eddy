e806ddb8916408e061af429772f279c61d3a8086
imeddyhi <156390561+imeddyhi@users.noreply.github.com>
2024-03-15 12:58:39 -0600

    Update sokoban-by-eddy(v.0.).py

--- sokoban-by-eddy(v.0.).py
+++ sokoban-by-eddy(v.0.).py
@@ -19,18 +19,20 @@ class Soko:
     def __init__(self):
         # Define el mapa de juego
         self.map =[
-            [3,3,3,3,3,3,3,3,3],
-            [3,4,4,4,4,4,4,4,3],
-            [3,4,0,4,4,1,4,2,3],
-            [3,4,4,4,4,4,4,4,3],
-            [3,3,3,3,3,3,3,3,3]
+            [3,3,3,3,3,3,3,3,3,3,3,3,3,3],
+            [3,4,4,4,4,4,4,4,4,4,4,4,4,3],
+            [3,4,0,4,4,4,4,4,4,4,1,4,2,3],
+            [3,4,4,4,4,1,4,4,4,4,4,4,4,3],
+            [3,4,4,4,4,4,4,1,4,4,4,4,4,3],
+            [3,4,4,4,4,4,4,4,4,4,4,4,4,3],
+            [3,3,3,3,3,3,3,3,3,3,3,3,3,3]
         ]
 
         # Definimos la posicion inicial del personaje
         self.charac_column = 2
         self.charac_fila = 2
-        # Definimos la posicion inicial de la caja
-        self.box_column = 4
+        # Definimos la posicion inicial de la caja #BORRAR------YAAAAA!#
+        self.box_column = 10
         self.box_fila = 2
 
     def printMap(self):
@@ -86,45 +88,77 @@ class Soko:
         # Actualiza la posicion del personaje
         self.charac_fila += 1
 
-    def mov5(self): #right, caja
-        # Donde estaba la caja pone un personaje
-        self.map[self.box_fila][self.box_column] = 0
-        # Donde estaba el piso pone la caja
-        self.map[self.box_fila][self.box_column + 1] = 1
-        # Actualiza la posicion de la caja
-        self.box_column += 1
+    def mov5(self): #right, caja [0,1,4]
+        # Donde estaba el personaje pone un piso, [4,0,4]
+        self.map[self.charac_fila][self.charac_column] = 4
+        # Donde estaba la caja pone al personaje, [4,0,4]
+        self.map[self.charac_fila][self.charac_column + 1] = 0
+        # Donde estaba el piso pone a la caja
+        self.map[self.charac_fila][self.charac_column + 2] = 1
+        # Actualiza la posicion del personaje
+        self.charac_column += 1
 
-    def mov6(self): #right, personaje-caja
-        # Donde estaba el personaje pone una caja
-        self.map[self.box_fila][self.box_column] = 0
-        # Donde estaba el piso pone la caja
-        self.map[self.box_fila][self.box_column + 1] = 1
-        # Actualiza la posicion de la caja
-        self.box_column += 1
+    def mov6(self): #left, caja [4,1,0]
+        # Donde estaba el personaje pone un piso, [4,0,4]
+        self.map[self.charac_fila][self.charac_column] = 4
+        # Donde estaba la caja pone al personaje
+        self.map[self.charac_fila][self.charac_column - 1] = 0
+        # Donde estaba el piso pone a la caja
+        self.map[self.charac_fila][self.charac_column - 2] = 1
+        # Actualiza la posicion del personaje
+        self.charac_column -= 1
+
+    def mov7(self): #up, caja [4,1,0]
+        # Donde estaba el personaje pone un piso, [4,0,4]
+        self.map[self.charac_fila][self.charac_column] = 4
+        # Donde estaba la caja pone al personaje
+        self.map[self.charac_fila - 1][self.charac_column] = 0
+        # Donde estaba el piso pone a la caja
+        self.map[self.charac_fila - 2][self.charac_column] = 1
+        # Actualiza la posicion del personaje
+        self.charac_fila -= 1
+
+    def mov8(self): #down, caja [4,1,0]
+        # Donde estaba el personaje pone un piso, [4,0,4]
+        self.map[self.charac_fila][self.charac_column] = 4
+        # Donde estaba la caja pone al personaje
+        self.map[self.charac_fila + 1][self.charac_column] = 0
+        # Donde estaba el piso pone a la caja
+        self.map[self.charac_fila + 2][self.charac_column] = 1
+        # Actualiza la posicion del personaje
+        self.charac_fila += 1
+
 
 #movimientos
     def right(self):
         # Movimiento 1: [0,4] -> [4,0]
         if self.map[self.charac_fila][self.charac_column] == 0 and self.map[self.charac_fila][self.charac_column + 1] == 4:
             self.mov1()
+        elif (self.map[self.charac_fila][self.charac_column] == 0 and self.map[self.charac_fila][self.charac_column + 1] == 1) and self.map[self.charac_fila][self.charac_column + 2] == 4:
+            self.mov5()
 
     def left(self):
         #mov 2: [4,0] -> [0,4]
         if self.map[self.charac_fila][self.charac_column] == 0 and self.map[self.charac_fila][self.charac_column - 1] == 4:
             self.mov2()
+        elif (self.map[self.charac_fila][self.charac_column] == 0 and self.map[self.charac_fila][self.charac_column - 1] == 1) and self.map[self.charac_fila][self.charac_column - 2] == 4:
+            self.mov6()
 
     def up(self):
         #mov 3: [4,4], [4,0] -> [4,0], [4,4]
         if self.map[self.charac_fila][self.charac_column] == 0 and self.map[self.charac_fila - 1][self.charac_column] == 4:
             self.mov3()
+        elif (self.map[self.charac_fila][self.charac_column] == 0 and self.map[self.charac_fila - 1][self.charac_column] == 1) and self.map[self.charac_fila - 2][self.charac_column] == 4:
+            self.mov7()
 
     def down(self):
         #mov 2: [4,0], [4,4] -> [4,4], [4,0]
         if self.map[self.charac_fila][self.charac_column] == 0 and self.map[self.charac_fila + 1][self.charac_column] == 4:
             self.mov4()
+        elif (self.map[self.charac_fila][self.charac_column] == 0 and self.map[self.charac_fila + 1][self.charac_column] == 1) and self.map[self.charac_fila + 2][self.charac_column] == 4:
+            self.mov8()
 
 
-    
 
     def play(self):
         while True:
